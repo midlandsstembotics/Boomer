d28e28b8c32c57c272eaf60d0d461b1a5f397f60
dannienuc <dannienuc@yahoo.com>
2020-01-24 12:25:36 -0500

    cleanup to split solution code and sample code

---
+++ Software/test stuff for class/sample_solution.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+"""sample solution for operation of the 'Boomer' robot produced by Midlands STEMbotics robotics club in Winnsboro SC.
+
+Copyright (c) 2019 Midlands STEMbotics robotics club, Winnsboro South Carolina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
+
+
+import cwiid
+import time
+import gpiozero
+
+boomer = gpiozero.Robot(left=(X,y), right=(x,y))
+
+#controller initialization
+print("Press and hold the 1 + 2 buttons on the WII remote at the same time:")
+time.sleep(2)
+wii = cwiid.Wiimote()
+print("Connection with remote established")
+wii.rpt_mode= cwiid.RPT_BTN
+
+
+
+#main command loop
+while True:
+    
+    button_status = wii.state["buttons"]
+
+
+
+
+"""
+    if(button_status & cwiid.BTN_UP):
+        boomer.forward(0.5)
+        continue
+
+    elif(button_status & cwiid.BTN_DOWN):
+        boomer.reverse(0.3)
+        continue
+
+    elif(buttons & cwiid.BTN_RIGHT):
+        boomer.right(0.5)
+        continue
+
+    elif(buttons & cwiid.BTN_LEFT):
+        boomer.left(0.5)
+        continue
+    elif(buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
+        print("\nClosing connection ...")
+        wii.rumble = 1
+        time.sleep(1)
+        wii.rumble = 0
+        exit(wii)
+        break
+    """
+
+"""
+def function1():
+
+    print ("called function 1")
+
+def function2():
+
+    print ("called function 2")
+
+def function3():
+
+    print ("called function 3")
+
+tokenDict = {"cat":function1, "dog":function2, "bear":function3}
+
+# simulate, say, lines read from a file
+
+lines = ["cat","bear","cat","dog"]
+
+for line in lines:
+
+   
+
+    # lookup the function to call for each line
+
+    functionToCall = tokenDict[line]
+
+    # and call it
+
+    functionToCall()
+"""
+
+
+"""
+def zero():
+    return 'zero'
+
+def one():
+    return 'one'
+
+def indirect(i):
+    switcher={
+        0:zero,
+        1:one,
+        2:lambda:'two'
+        }
+    func=switcher.get(i,lambda :'Invalid')
+    return func()
+
+indirect(4)
+"""
