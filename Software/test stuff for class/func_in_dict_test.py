706567a83c88933075b16d61b29b65b8de0f0bf0
dannienuc <dannienuc@yahoo.com>
2020-01-24 11:12:07 -0500

    moved stest stuff to it's own folder

--- Software/test stuff for class/func_in_dict_test.py
+++ Software/test stuff for class/func_in_dict_test.py
@@ -2,15 +2,15 @@ import string
 
 def function1():
 
-    print "called function 1"
+    print ("called function 1")
 
 def function2():
 
-    print "called function 2"
+    print ("called function 2")
 
 def function3():
 
-    print "called function 3"
+    print ("called function 3")
 
 tokenDict = {"cat":function1, "dog":function2, "bear":function3}
 
