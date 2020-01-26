b0c791d8ab9119e3e3f978ebdfc4d787c35073e0
Dan Campbell <dannienuc@yahoo.com>
2020-01-26 08:53:30 -0500

    set up specific test file for verifying cwiid functionality

---
+++ Software/cwiidtest.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+"""sample solution for operation of the 'Boomer' robot produced by Midlands STEMbotics robotics club in Winnsboro SC.
+
+Copyright (c) 2019 Midlands STEMbotics robotics club, Winnsboro South Carolina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
+
+
+import cwiid
+import time
+import gpiozero
+
+boomer = gpiozero.Robot(left=(X,y), right=(x,y))
+
+#controller initialization
+print("Press and hold the 1 + 2 buttons on the WII remote at the same time:")
+time.sleep(2)
+wii = cwiid.Wiimote()
+print("Connection with remote established")
+wii.rpt_mode= cwiid.RPT_BTN
+
+
+
+#main command loop
+while True:
+    
+    button_status = wii.state["buttons"]
+
+    if(button_status & cwiid.BTN_UP):
+        #boomer.forward(0.5)
+        print("up button pressed")
+        continue
+
+    elif(button_status & cwiid.BTN_DOWN):
+        #boomer.reverse(0.3)
+        print("down button pressed")
+        continue
+
+    elif(buttons & cwiid.BTN_RIGHT):
+        #boomer.right(0.5)
+        print("right button pressed")
+        continue
+
+    elif(buttons & cwiid.BTN_LEFT):
+        #boomer.left(0.5)
+        print("left button pressed")
+        continue
+    elif(buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
+        print("\nClosing connection ...")
+        wii.rumble = 1
+        time.sleep(1)
+        wii.rumble = 0
+        exit(wii)
+        break
+    
