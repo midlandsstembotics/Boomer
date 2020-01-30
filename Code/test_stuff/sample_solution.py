a1f76e6ba5777819dc6749ef73f51c3d4da2d3d5
Dan Campbell <dannienuc@yahoo.com>
2020-01-30 08:58:37 -0500

    misc code cleanup in testing branch

---
+++ Code/test_stuff/sample_solution.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+#sample solution for operation of the 'Boomer' robot produced by 
+# Midlands STEMbotics robotics club in Winnsboro SC.
+#Copyright (c) 2019 Midlands STEMbotics robotics club, Winnsboro South Carolina
+
+import cwiid
+import time
+import gpiozero
+
+#set up harware links for the two motors using the indicated GPIOS.
+#NOTE: if SPI is active, must swap GPIO 20 to a different pin.
+boomer = gpiozero.Robot(left=(17,27), right=(26,20))
+
+#controller initialization
+print("Press and hold the 1 + 2 buttons on the WII remote at the same time:")
+time.sleep(5)
+wii = cwiid.Wiimote()
+print("Connection with remote established")
+wii.rpt_mode= cwiid.RPT_BTN
+
+
+
+#main command loop
+while True:
+    
+    button_status = wii.state["buttons"]
+
+    if(button_status & cwiid.BTN_UP):
+        boomer.forward(0.3)
+        continue
+
+    elif(button_status & cwiid.BTN_DOWN):
+        boomer.reverse(0.3)
+        continue
+
+    elif(button_status & cwiid.BTN_RIGHT):
+        boomer.right(0.3)
+        continue
+
+    elif(button_status & cwiid.BTN_LEFT):
+        boomer.left(0.3)
+        continue
+    elif(button_status - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
+        print("\nClosing connection ...")
+        wii.rumble = 1
+        time.sleep(1)
+        wii.rumble = 0
+        exit(wii)
+        break
+
