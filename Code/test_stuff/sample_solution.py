704c93df9520cb741a3e532290d01e33118b2e4d
Dan Campbell <dannienuc@yahoo.com>
2020-01-30 09:18:44 -0500

    added degug prints for button presses

--- Code/test_stuff/sample_solution.py
+++ Code/test_stuff/sample_solution.py
@@ -20,25 +20,28 @@ print("Connection with remote established")
 wii.rpt_mode= cwiid.RPT_BTN
 
 
-
 #main command loop
 while True:
     
     button_status = wii.state["buttons"]
 
     if(button_status & cwiid.BTN_UP):
+        print("up button pressed")
         boomer.forward(0.3)
         continue
 
     elif(button_status & cwiid.BTN_DOWN):
+        print("down button pressed")
         boomer.reverse(0.3)
         continue
 
     elif(button_status & cwiid.BTN_RIGHT):
+        print("right button pressed")
         boomer.right(0.3)
         continue
 
     elif(button_status & cwiid.BTN_LEFT):
+        print("left button pressed")
         boomer.left(0.3)
         continue
     elif(button_status - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
