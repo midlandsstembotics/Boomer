7e0a86ac135ea89fa596716146e708e9d252d0cb
dannienuc <dannienuc@yahoo.com>
2020-01-30 16:37:05 -0500

    Update sample_solution.py

--- Code/test_stuff/sample_solution.py
+++ Code/test_stuff/sample_solution.py
@@ -28,25 +28,25 @@ while True:
     if(button_status & cwiid.BTN_UP):
         print("up button pressed")
         boomer.forward(0.3)
-        time.sleep(0.1)
+        time.sleep(0.2)
         continue
 
     elif(button_status & cwiid.BTN_DOWN):
         print("down button pressed")
         boomer.backward(0.3)
-        time.sleep(0.1)
+        time.sleep(0.2)
         continue
 
     elif(button_status & cwiid.BTN_RIGHT):
         print("right button pressed")
         boomer.right(0.3)
-        time.sleep(0.1)
+        time.sleep(0.2)
         continue
 
     elif(button_status & cwiid.BTN_LEFT):
         print("left button pressed")
         boomer.left(0.3)
-        time.sleep(0.1)
+        time.sleep(0.2)
         continue
     elif(button_status - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
         print("\nClosing connection ...")
