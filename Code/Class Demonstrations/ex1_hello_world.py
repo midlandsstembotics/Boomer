7584bd95af2a3b35a98bf398bccd6b44ee3e128f
Dan Campbell <dannienuc@yahoo.com>
2020-01-31 11:12:28 -0500

    updated pictures

---
+++ Code/Class Demonstrations/ex1_hello_world.py
@@ -0,0 +1,41 @@
+
+
+#This is a comment. Comments can be placed anywhere using the '#'
+# symbol.......
+
+"""This is also a comment. an entire block of text can be a comment
+as long as it is surrounded by three quotation marks"""
+
+#Next we will look at one of the most common statements you can use,
+#the print() statement.
+
+#look at each of these statements, and guess what the output will 
+#be:
+
+a = "Hello World" #this creates a variable called 'a' and assigns it the value of the text "Hello World"
+print a 
+
+#The print statement/function is used to send information to the terminal. The terminal is a fancy 
+#way of describing the computer's screen. What do you think the output on the terminal's screen will be?
+#what about this???
+
+print "Hello World"
+
+#do you think that there will be any difference between this output, and the first print statement?
+#let's try a third way.......
+
+print("Hello World")
+
+#Take a guess as to what this third print statement does?
+#One more, and this one may be a bit confusing.....
+
+foo = 100
+print"If you guessed this correctly, your grade is %s", foo
+
+#That one was pretty neat, right?
+
+"""Some questions to think about:
+1)Based on the file above, what is a variable?
+2)Is the '=' used here in the same way that you would use it in math class?
+3)For that last example, why do you think it might be useful?
+
