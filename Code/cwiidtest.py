30095a4e28188837060f0908548b977bd61ae9bb
Dan Campbell <dannienuc@yahoo.com>
2020-01-31 09:34:18 -0500

    pin numbering on solution and cwiid refactor for class

--- Code/cwiidtest.py
+++ Code/cwiidtest.py
@@ -1,24 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-#sample solution for operation of the 'Boomer' robot produced by 
-# Midlands STEMbotics robotics club in Winnsboro SC.
-#Copyright (c) 2019 Midlands STEMbotics robotics club, Winnsboro South Carolina
+"""Test of the CWIID python3 library used in the 'Boomer' robot produced 
+    by Midlands STEMbotics robotics club in Winnsboro SC.
+Copyright (c) 2019 Midlands STEMbotics robotics club, Winnsboro South Carolina
+"""
+
 
 
 import cwiid
 import time
 import gpiozero
 
-boomer = gpiozero.Robot(left=(27,17), right=(26,13))
-
 #controller initialization
 print("Press and hold the 1 + 2 buttons on the WII remote at the same time:")
-time.sleep(2)
+time.sleep(5)
 wii = cwiid.Wiimote()
 print("Connection with remote established")
 wii.rpt_mode= cwiid.RPT_BTN
-
-
 
 #main command loop
 while True:
@@ -26,29 +24,29 @@ while True:
     button_status = wii.state["buttons"]
 
     if(button_status & cwiid.BTN_UP):
-        boomer.forward(0.5)
         print("up button pressed")
+        #time.sleep(0.1)
         continue
 
     elif(button_status & cwiid.BTN_DOWN):
-        boomer.reverse(0.3)
         print("down button pressed")
+        #time.sleep(0.1)
         continue
 
-    elif(buttons & cwiid.BTN_RIGHT):
-        boomer.right(0.5)
+    elif(button_status & cwiid.BTN_RIGHT):
         print("right button pressed")
+        #time.sleep(0.1)
         continue
 
-    elif(buttons & cwiid.BTN_LEFT):
-        boomer.left(0.5)
+    elif(button_status & cwiid.BTN_LEFT):
         print("left button pressed")
+        #time.sleep(0.1)
         continue
-    elif(buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
+    elif(button_status - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
         print("\nClosing connection ...")
         wii.rumble = 1
         time.sleep(1)
-        wii.rumble = 0
+        wii.rumble = 1
         exit(wii)
         break
     
